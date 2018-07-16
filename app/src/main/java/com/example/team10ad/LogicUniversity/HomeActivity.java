42b33952ac2e85ba149133d8eb927ead65ed1db3
Htet Wai Yan Htet Wai Yan <e0284026@u.nus.edu>
2018-07-16 21:16:56 +0800

    hod Drawer Menu
    Hod Drawer Menu

--- app/src/main/java/com/example/team10ad/LogicUniversity/HomeActivity.java
+++ app/src/main/java/com/example/team10ad/LogicUniversity/HomeActivity.java
@@ -44,6 +44,16 @@ public class HomeActivity extends AppCompatActivity
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        NavigationView nav = findViewById(R.id.nav_view);
+
+        // Clerk menu
+        //nav.getMenu().clear();
+        //nav.inflateMenu(R.menu.activity_home_drawer);
+
+        // HOD menu
+        nav.getMenu().clear();
+        nav.inflateMenu(R.menu.activity_home_hod);
+
         setupPieChart();
         //link to requisition list screen
         LinearLayout req=(LinearLayout)findViewById(R.id.requisitionID);
