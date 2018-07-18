191f057f479075b3292c5eee90ae5a6e257b84bf
chitsushine <chitsushine.utycc@gmail.com>
2018-07-18 16:51:33 +0800

    Conflict Resolve 4:51 PM

--- app/src/main/java/com/example/team10ad/LogicUniversity/Report1Fragment.java
+++ app/src/main/java/com/example/team10ad/LogicUniversity/Report1Fragment.java
@@ -1,13 +1,14 @@
 package com.example.team10ad.LogicUniversity;
 
-
+import android.content.Context;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import com.example.team10ad.team10ad.R;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.Legend;
@@ -23,8 +24,23 @@ import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link Report1Fragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link Report1Fragment#newInstance} factory method to
+ * create an instance of this fragment.
  */
-public class Report1Fragment extends Fragment{
+public class Report1Fragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
 
     private BarChart chart, layout;
     float barWidth;
@@ -35,6 +51,32 @@ public class Report1Fragment extends Fragment{
         // Required empty public constructor
     }
 
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Report1Fragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Report1Fragment newInstance(String param1, String param2) {
+        Report1Fragment fragment = new Report1Fragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -121,5 +163,38 @@ public class Report1Fragment extends Fragment{
         leftAxis.setSpaceTop(35f);
         leftAxis.setAxisMinimum(0f);
         return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
     }
 }
