a0376d01797be6decc481a46017f22ed767be45c
Wint <wintydnhtet@gmail.com>
2018-07-20 15:58:09 +0800

    hod dashboard

--- app/src/main/java/com/example/team10ad/LogicUniversity/DepartmentHead/HodDashboardFragment.java
+++ app/src/main/java/com/example/team10ad/LogicUniversity/DepartmentHead/HodDashboardFragment.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -45,6 +47,68 @@ public class HodDashboardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.fragment_hod_dashboard, container, false);
+        //approve
+        CardView cardapp=(CardView)view.findViewById(R.id.cardapprove);
+        cardapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                HodReqApproveRejectFragment hodapprej=new HodReqApproveRejectFragment();
+                FragmentManager fragmentManager=getFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.content_frame, hodapprej).commit();
+            }
+        });
+        //delegate
+        CardView carddelegate=(CardView)view.findViewById(R.id.carddelegate);
+        carddelegate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DelegateAuthorityFragment hoddelegate=new DelegateAuthorityFragment();
+                FragmentManager fragmentManager=getFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.content_frame, hoddelegate).commit();
+            }
+        });
+        //assign
+        CardView cardassign=(CardView)view.findViewById(R.id.cardassign);
+        cardassign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AssignDepRepFragment hodassign=new AssignDepRepFragment();
+                FragmentManager fragmentManager=getFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.content_frame, hodassign).commit();
+            }
+        });
+        //tracking
+        CardView cardtracking=(CardView)view.findViewById(R.id.cardtracking);
+        cardtracking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ReqListForTrackingOrder hodtracking=new ReqListForTrackingOrder();
+                FragmentManager fragmentManager=getFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.content_frame, hodtracking).commit();
+            }
+        });
+        //change collection point
+        CardView cardcollect=(CardView)view.findViewById(R.id.cardcollect);
+        cardcollect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ChangeCollectionPoint hodcollect=new ChangeCollectionPoint();
+                FragmentManager fragmentManager=getFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.content_frame, hodcollect).commit();
+            }
+        });
+        //report
+        CardView cardreport=(CardView)view.findViewById(R.id.cardreport);
+        cardreport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                HODReport hodreport=new HODReport();
+                FragmentManager fragmentManager=getFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.content_frame, hodreport).commit();
+            }
+        });
+
+
 
         return view;
     }
