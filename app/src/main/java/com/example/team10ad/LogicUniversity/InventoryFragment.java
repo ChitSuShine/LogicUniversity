516714d65733cac3893c20aa486f3bfcc5ac847c
Wint <wintydnhtet@gmail.com>
2018-07-29 18:33:44 +0800

    ui

--- app/src/main/java/com/example/team10ad/LogicUniversity/InventoryFragment.java
+++ app/src/main/java/com/example/team10ad/LogicUniversity/InventoryFragment.java
@@ -3,6 +3,7 @@ package com.example.team10ad.LogicUniversity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -12,9 +13,12 @@ import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -38,6 +42,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+import de.mrapp.android.dialog.MaterialDialog;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -170,6 +175,9 @@ public class InventoryFragment extends Fragment {
             }
         });
         dialog.show();
+        Window window = dialog.getWindow();
+        window.setLayout(LinearLayout.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.TYPE_APPLICATION_ATTACHED_DIALOG);
     }
 
     public void processDiscrepency()
